9b2c7fff13b6bcb9051b47765895b3809780a98d
JaimeCY <jaimecy2323@gmail.com>
2025-07-01 10:26:06 +0200

    feat: versión en español con nombres de países y búsqueda en español

--- libs/ngx-material-intl-tel-input-lib/src/index.ts
+++ libs/ngx-material-intl-tel-input-lib/src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/ngx-material-intl-tel-input-lib/ngx-material-intl-tel-input-lib.component';
 export * from './lib/types/text-labels.type';
 export * from './lib/enums/country-iso.enum';
+export * from './lib/data/country-code-spanish';
